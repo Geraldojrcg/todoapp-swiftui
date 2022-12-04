d8e84b8d796a2541495d668da8312cf3177d39d4
Geraldo Jr <geraldojrcg@gmail.com>
2022-12-04 09:46:01 -0300

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Geraldo Júnior on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
